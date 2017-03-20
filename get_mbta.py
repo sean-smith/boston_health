2f5fd3ba9ffa8cce9111ccf4bcf9a77b129dfa41
veev <vivyee@bu.edu>
2017-03-20 18:09:05 -0400

    get mbta data

--- get_mbta.py
+++ get_mbta.py
@@ -1,6 +1,6 @@
 # RUN IN PYTHON 3
 import json
-import requests
+import urllib.request
 
 # API url for nutrition program data
 url = 'http://realtime.mbta.com/developer/api/v2/routes?api_key=JRz51UahAUCXkQHxPQ40fA&format=json'
@@ -16,14 +16,14 @@ stop_url = 'http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=JRz51
 stop_urls = {route:"{}&route={}&format=json".format(stop_url, route[1]) for route in routes}
 stop_responses = {route:urllib.request.urlopen(stop_urls[route]).read().decode("utf-8") for route in stop_urls}
 
-for route, response in stop_responses:
+for route, response in stop_responses.items():
     stops_by_route = {}
 
     mode, route_id = route
     stops_by_route['mode'] = mode
     stops_by_route['path'] = json.loads(response)
 
-    result[route_id] = stops_by_route
+    result[route_id] = json.dumps(stops_by_route)
 
 
 with open('programs.json', 'w') as f:
